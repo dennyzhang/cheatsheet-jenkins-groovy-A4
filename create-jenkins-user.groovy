32269661d3b1cfac300c6f93f51603045e490c8a
Denny Zhang <denny.zhang001@gmail.com>
2019-03-14 16:09:14 -0700

    update groovy script

--- create-jenkins-user.groovy
+++ create-jenkins-user.groovy
@@ -1,4 +1,16 @@
+//-------------------------------------------------------------------
+// @copyright 2018 DennyZhang.com
+// Licensed under MIT
+// https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: string-to-json.groovy
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
 // https://gist.github.com/hayderimran7/50cb1244cc1e856873a4
+// --
+// Created : <2018-04-20>
+// Updated: Time-stamp: <2019-03-14 15:29:43>
+//-------------------------------------------------------------------
 
 import jenkins.model.*
 import hudson.security.*
