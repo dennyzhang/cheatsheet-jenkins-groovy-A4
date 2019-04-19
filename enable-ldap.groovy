3ac7b6bb9c26c87754e8d163a8ae6f8b1533e9d5
Denny Zhang <denny.zhang001@gmail.com>
2019-04-19 10:21:59 -0700

    jenkins enable ldap

---
+++ enable-ldap.groovy
@@ -0,0 +1,59 @@
+import jenkins.model.*
+
+import hudson.security.LDAPSecurityRealm
+import hudson.security.HudsonPrivateSecurityRealm
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
+import hudson.util.Secret
+import jenkins.model.IdStrategy
+import jenkins.security.plugins.ldap.LDAPConfiguration
+import net.sf.json.JSONObject
+
+// https://wiki.jenkins.io/display/JENKINS/LDAP+Plugin
+if(!binding.hasVariable('ldap_settings')) {
+   ldap_settings = [
+      'server': 'ldaps://ldaps.mycompany.com:636',
+      'rootDN': 'DC=mycompany,DC=com',
+   ]
+}
+if(!(ldap_settings instanceof Map)) {
+   throw new Exception('ldap_settings must be a Map.')
+}
+
+ldap_settings = ldap_settings as JSONObject
+
+if(!(Jenkins.instance.securityRealm instanceof LDAPSecurityRealm)) {
+   LDAPConfiguration conf = new LDAPConfiguration(
+      ldap_settings.optString('server'),
+      ldap_settings.optString('rootDN'),
+      ldap_settings.optBoolean('inhibitInferRootDN'),
+      ldap_settings.optString('managerDN'),
+      Secret.fromString(ldap_settings.optString('managerPasswordSecret')))
+
+   conf.userSearchBase = ldap_settings.optString('userSearchBase')
+   conf.userSearch = ldap_settings.optString('userSearch', LDAPSecurityRealm.DescriptorImpl.DEFAULT_USER_SEARCH)
+   conf.groupSearchBase = ldap_settings.optString('groupSearchBase')
+   conf.groupSearchFilter = ldap_settings.optString('groupSearchFilter')
+   conf.environmentProperties = (ldap_settings.opt('environmentProperties')?:[:]).collect { k, v ->
+      new LDAPSecurityRealm.EnvironmentProperty(k.toString(), v.toString())
+   } as LDAPSecurityRealm.EnvironmentProperty[]
+   conf.displayNameAttributeName = ldap_settings.optString('displayNameAttributeName', LDAPSecurityRealm.DescriptorImpl.DEFAULT_DISPLAYNAME_ATTRIBUTE_NAME)
+   conf.mailAddressAttributeName = ldap_settings.optString('mailAddressAttributeName', LDAPSecurityRealm.DescriptorImpl.DEFAULT_MAILADDRESS_ATTRIBUTE_NAME)
+
+   List<LDAPConfiguration> configurations = [conf]
+   Jenkins.instance.securityRealm = new LDAPSecurityRealm(
+      configurations,
+      ldap_settings.optBoolean('disableMailAddressResolver'),
+      null,
+      IdStrategy.CASE_INSENSITIVE,
+      IdStrategy.CASE_INSENSITIVE)
+
+   def strategy = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
+   strategy.setAllowAnonymousRead(true)
+   Jenkins.instance.setAuthorizationStrategy(strategy)
+
+   Jenkins.instance.save()
+   println 'Security realm set to LDAP.'
+}
+else {
+   println 'Nothing changed.  LDAP security realm already configured.'
+}
---
+++ matrix-authorization-strategy.groovy
@@ -0,0 +1,169 @@
+// http://www.tothenew.com/blog/jenkins-implementing-project-based-matrix-authorization-strategy/
+/*
+   Configure matrix authorization strategy with permissions for users and
+   groups.  This script is idempotent and will only change configuration if
+   necessary.
+
+   Example configuration:
+       authz_strategy_config = [
+           strategy: 'GlobalMatrixAuthorizationStrategy',
+           user_permissions: [
+               anonymous: ['Job Discover'],
+               authenticated: ['Overall Read', 'Job Read', 'View Read'],
+               admin: ['Overall Administer']
+           ]
+       ]
+
+   Available Authorization Strategies:
+       GlobalMatrixAuthorizationStrategy
+       ProjectMatrixAuthorizationStrategy
+
+   Available user permissions:
+       Overall Administer
+       Overall Read
+       Agent Configure
+       Agent Delete
+       Agent Create
+       Agent Disconnect
+       Agent Connect
+       Agent Build
+       Agent Provision
+       Run Delete
+       Run Update
+       Job Create
+       Job Delete
+       Job Configure
+       Job Read
+       Job Discover
+       Job Build
+       Job Workspace
+       Job Cancel
+       SCM Tag
+       Credentials Create
+       Credentials Update
+       Credentials View
+       Credentials Delete
+       Credentials ManageDomains
+       Job Move
+       View Create
+       View Delete
+       View Configure
+       View Read
+       Run Replay
+
+   "Job ViewStatus" permission becomes available after installing the
+   embeddable build status plugin.
+ */
+import jenkins.model.*
+
+import hudson.security.GlobalMatrixAuthorizationStrategy
+import hudson.security.Permission
+import hudson.security.ProjectMatrixAuthorizationStrategy
+import jenkins.model.Jenkins
+
+class Helper implements Serializable {
+   static String shortName(Permission p) {
+      p.id.tokenize('.')[-2..-1].join(' ')
+	 .replace('Hudson','Overall')
+	 .replace('Computer', 'Agent')
+	 .replace('Item', 'Job')
+	 .replace('CredentialsProvider', 'Credentials')
+   }
+
+   static Map getCurrentPermissions(Map config = [:]) {
+      Map currentPermissions = [:].withDefault { [].toSet() }
+      if(!('getGrantedPermissions' in Jenkins.instance.authorizationStrategy.metaClass.methods*.name.sort().unique())) {
+	 return currentPermissions
+      }
+      Closure merger = { Map nmap, Map m ->
+	 m.each { k, v ->
+	    nmap[k] += v
+	 }
+      }
+      Jenkins.instance.authorizationStrategy.grantedPermissions.collect { permission, userList ->
+	 userList.collect { user ->
+	    [ (user): shortName(permission) ]
+	 }
+      }.flatten().each merger.curry(currentPermissions)
+      currentPermissions
+   }
+
+   static boolean isConfigurationEqual(Map config) {
+      Map currentPermissions = getCurrentPermissions(config)
+      Jenkins.instance.authorizationStrategy.class.name.endsWith(config['strategy']) &&
+      !(false in config['user_permissions'].collect { k, v -> currentPermissions[k] == v.toSet() }) &&
+      currentPermissions.keySet() == config['user_permissions'].keySet()
+   }
+
+   static boolean isValidConfig(def config, List<String> validPermissions) {
+      Map currentPermissions = getCurrentPermissions()
+      config instanceof Map &&
+      config.keySet().containsAll(['strategy', 'user_permissions']) &&
+      config['strategy'] &&
+      config['strategy'] instanceof String &&
+      config['strategy'] in ['GlobalMatrixAuthorizationStrategy', 'ProjectMatrixAuthorizationStrategy'] &&
+      config['user_permissions'] &&
+      !(false in config['user_permissions'].collect { k, v ->
+	 k instanceof String &&
+	 (v instanceof List || v instanceof Set) &&
+	 !(false in v.collect {
+	    validPermissions.contains(it)
+	 })
+      })
+   }
+}
+
+Map<String, Permission> permissionIds = Permission.all.findAll { permission ->
+   List<String> nonConfigurablePerms = ['RunScripts', 'UploadPlugins', 'ConfigureUpdateCenter']
+   permission.enabled &&
+      !permission.id.startsWith('hudson.security.Permission') &&
+      !(true in nonConfigurablePerms.collect { permission.id.endsWith(it) })
+}.collect { permission ->
+   [ (Helper.shortName(permission)): permission ]
+}.sum()
+
+/**
+ * MAIN EXECUTION
+ */
+
+if(!binding.hasVariable('authz_strategy_config')) {
+   authz_strategy_config = [
+      strategy: 'GlobalMatrixAuthorizationStrategy',
+      user_permissions: [
+	 'authenticated': ['Overall Read'],
+	 'zdenny': ['Overall Administer']
+      ]
+   ]
+}
+
+if(!Helper.isValidConfig(authz_strategy_config, permissionIds.keySet().toList())) {
+   println([
+      'Skip configuring matrix authorization strategy because no valid config was provided.',
+      'Available Authorization Strategies:\n    GlobalMatrixAuthorizationStrategy\n    ProjectMatrixAuthorizationStrategy',
+      "Available Permissions:\n    ${permissionIds.keySet().join('\n    ')}"
+   ].join('\n'))
+   return
+}
+
+if(Helper.isConfigurationEqual(authz_strategy_config)) {
+   println "Nothing changed.  ${authz_strategy_config['strategy']} authorization strategy already configured."
+   return
+}
+
+println "Configuring authorization strategy ${authz_strategy_config['strategy']}"
+
+def authz_strategy = Class.forName("hudson.security.${authz_strategy_config['strategy']}").newInstance()
+
+// build the permissions in the strategy
+authz_strategy_config['user_permissions'].each { user, permissions ->
+   permissions.each { p ->
+      authz_strategy.add(permissionIds[p], user)
+      println "    For user ${user} grant permission ${p}."
+   }
+}
+
+// configure global authorization
+Jenkins.instance.authorizationStrategy = authz_strategy
+
+// save settings to persist across restarts
+Jenkins.instance.save()
