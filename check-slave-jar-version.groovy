40c6f24ca78605b5912d65ec7634283aec2f014b
Denny Zhang <denny.zhang001@gmail.com>
2019-05-01 11:29:48 -0700

    jenkins check slave

---
+++ check-slave-jar-version.groovy
@@ -0,0 +1,28 @@
+#!groovy
+//-------------------------------------------------------------------
+// @copyright 2018 DennyZhang.com
+// Licensed under MIT
+// https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: string-to-json.groovy
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
+// --
+// Created : <2018-04-20>
+// Updated: Time-stamp: <2019-05-01 11:29:26>
+//-------------------------------------------------------------------
+// https://gist.github.com/ctran/8f82adedaa23ea924c52
+import jenkins.model.*
+import hudson.remoting.Launcher
+import hudson.slaves.SlaveComputer
+
+def expectedVersion = Launcher.VERSION
+for (computer in Jenkins.instance.getComputers()) {
+  if (! (computer instanceof SlaveComputer)) continue
+  if (!computer.getChannel()) continue
+	
+  def version = computer.getSlaveVersion()
+  if (!expectedVersion.equals(version)) {
+    println "${computer.name} - expected ${expectedVersion} but got ${version}"
+  }
+}
---
+++ jenkins-url.groovy
@@ -0,0 +1,23 @@
+#!groovy
+//-------------------------------------------------------------------
+// @copyright 2018 DennyZhang.com
+// Licensed under MIT
+// https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: string-to-json.groovy
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
+// --
+// Created : <2018-04-20>
+// Updated: Time-stamp: <2019-05-01 11:15:58>
+//-------------------------------------------------------------------
+// https://gist.github.com/fishi0x01/7c2d29afbaa0f16126eb4d4b35942f76
+/*
+ * This script configures the Jenkins base URL.
+ */
+
+import jenkins.model.JenkinsLocationConfiguration
+
+JenkinsLocationConfiguration location = Jenkins.instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
+location.url = 'https://jenkins-as-code-poc.devtail.io/'
+location.save()
