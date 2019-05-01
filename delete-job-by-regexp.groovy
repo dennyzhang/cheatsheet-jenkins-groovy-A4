b2925bea14057aea0a5221a532cad471fe51ffce
Denny Zhang <denny.zhang001@gmail.com>
2019-05-01 11:14:59 -0700

    groovy to delete jenkins job

---
+++ delete-job-by-regexp.groovy
@@ -0,0 +1,24 @@
+#!groovy
+//-------------------------------------------------------------------
+// @copyright 2018 DennyZhang.com
+// Licensed under MIT
+// https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: string-to-json.groovy
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
+// --
+// Created : <2018-04-20>
+// Updated: Time-stamp: <2019-05-01 11:13:41>
+//-------------------------------------------------------------------
+// https://gist.github.com/nextrevision/d11b2df4e8b229c6855b
+import jenkins.model.*
+
+def matchedJobs = Jenkins.instance.items.findAll { job ->
+    job.name =~ /my_regex_here/
+}
+    
+matchedJobs.each { job ->
+    println job.name
+    //job.delete()
+}
