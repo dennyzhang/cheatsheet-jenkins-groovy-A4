00dca87cb840d17ab4dbb0695bc4155becb7bdef
Denny Zhang <denny.zhang001@gmail.com>
2019-05-01 14:00:53 -0700

    groovy run command

---
+++ execute-command.groovy
@@ -0,0 +1,21 @@
+#!groovy
+//-------------------------------------------------------------------
+// @copyright 2018 DennyZhang.com
+// Licensed under MIT
+// https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: string-to-json.groovy
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
+// --
+// Created : <2018-04-20>
+// Updated: Time-stamp: <2019-05-01 14:00:23>
+//-------------------------------------------------------------------
+// https://gist.github.com/katta/5465317
+def command = "date"
+def proc = command.execute()
+proc.waitFor()     
+
+println "Process exit code: ${proc.exitValue()}"
+println "Std Err: ${proc.err.text}"
+println "Std Out: ${proc.in.text}" 
