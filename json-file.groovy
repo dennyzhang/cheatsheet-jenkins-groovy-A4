328f1bff068a79e448528fcaae9951027ab7c417
Denny Zhang <denny.zhang001@gmail.com>
2019-05-01 11:11:09 -0700

    more groovy scripts

---
+++ json-file.groovy
@@ -0,0 +1,30 @@
+#!groovy
+//-------------------------------------------------------------------
+// @copyright 2018 DennyZhang.com
+// Licensed under MIT
+// https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: string-to-json.groovy
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
+// --
+// Created : <2018-04-20>
+// Updated: Time-stamp: <2019-05-01 11:10:32>
+//-------------------------------------------------------------------
+// https://gist.github.com/keyle/723181f3a9a59a3f7652
+
+package utils
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+class HDD {
+
+    static save(Object content, String filePath) {
+        new File(filePath).write(new JsonBuilder(content).toPrettyString())
+    }
+
+    static Object load(String filePath) {
+        return new JsonSlurper().parseText(new File(filePath).text)
+    }
+}
---
+++ regexp-match.groovy
@@ -0,0 +1,21 @@
+#!groovy
+//-------------------------------------------------------------------
+// @copyright 2018 DennyZhang.com
+// Licensed under MIT
+// https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: string-to-json.groovy
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
+// --
+// Created : <2018-04-20>
+// Updated: Time-stamp: <2019-05-01 11:04:49>
+//-------------------------------------------------------------------
+// https://gist.github.com/EwanDawson/2407215
+matcher = "Hello world v1.01" =~ /.* v(\S*)/
+if (matcher.matches()) version = matcher[0][1]
+assert version == "1.01"
+
+// We can make this a little tidier using the 'with' method
+version = ("Hello world v1.01" =~ /.* v(\S*)/).with { matches() ? it[0][1] : null }
+assert version == "1.01"
