af9fa4831f3485d4959b5361cc5a81ac89347021
Denny Zhang <denny.zhang001@gmail.com>
2019-05-09 11:43:33 -0700

    jenkins configure url

--- jenkins-url.groovy
+++ jenkins-url.groovy
@@ -9,7 +9,7 @@
 // Link: https://cheatsheet.dennyzhang.com/cheatsheet-jenkins-groovy-a4
 // --
 // Created : <2018-04-20>
-// Updated: Time-stamp: <2019-05-01 16:52:52>
+// Updated: Time-stamp: <2019-05-09 11:43:15>
 //-------------------------------------------------------------------
 // https://gist.github.com/fishi0x01/7c2d29afbaa0f16126eb4d4b35942f76
 /*
@@ -21,3 +21,18 @@ import jenkins.model.JenkinsLocationConfiguration
 JenkinsLocationConfiguration location = Jenkins.instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
 location.url = 'https://jenkins-as-code-poc.devtail.io/'
 location.save()
+
+// Solution 2
+// https://github.com/docksal/service-jenkins/blob/master/groovy/default-config.groovy
+import jenkins.model.*
+import hudson.security.*
+
+// Get environment variables.
+def env = System.getenv()
+
+// Change location configuration for admin address and Jenkins URL.
+// Currently this will override settings changed via Jenkins UI.
+def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+jenkinsLocationConfiguration.setAdminAddress(env.JENKINS_EMAIL)
+jenkinsLocationConfiguration.setUrl(env.JENKINS_URL)
+jenkinsLocationConfiguration.save()
